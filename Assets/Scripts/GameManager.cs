55eb3f343e6d7e4118491961df6f700571cbaef5
Smugpng <zikadika2@gmail.com>
2023-11-04 16:12:19 -0400

    Added EnemyTwo

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public GameObject enemyOnePrefab;
+    public GameObject enemyTwoPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("CreateEnemyOne", 1.0f, 3.0f);
+        InvokeRepeating("CreateEnemyTwo", 1.0f, 3.0f);
     }
 
     // Update is called once per frame
@@ -22,4 +24,9 @@ public class GameManager : MonoBehaviour
     {
         Instantiate(enemyOnePrefab, new Vector3(Random.Range(-8, 8), 7, 0), Quaternion.identity);
     }
+    void CreateEnemyTwo()
+    {
+        Instantiate(enemyTwoPrefab, new Vector3(Random.Range(-6, 6), 7, 0), Quaternion.identity);
+    }
+
 }
